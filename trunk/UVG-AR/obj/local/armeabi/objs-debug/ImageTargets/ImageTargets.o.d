445d0aa79ba7fc7f402555e60fe006121d7b7875
bol12114@uvg.edu.gt <bol12114@uvg.edu.gt@06752311-9fa4-b76d-36db-f0a8915f7b40>
2013-11-14 03:38:41 +0000

    UVG-AR

---
+++ trunk/UVG-AR/obj/local/armeabi/objs-debug/ImageTargets/ImageTargets.o.d
@@ -0,0 +1,168 @@
+obj/local/armeabi/objs-debug/ImageTargets/ImageTargets.o: \
+ jni/ImageTargets.cpp \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/jni.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/assert.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h \
+ C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h \
+ jni/../../../build/include/QCAR/QCAR.h \
+ jni/../../../build/include/QCAR/System.h \
+ jni/../../../build/include/QCAR/CameraDevice.h \
+ jni/../../../build/include/QCAR/NonCopyable.h \
+ jni/../../../build/include/QCAR/VideoMode.h \
+ jni/../../../build/include/QCAR/CameraCalibration.h \
+ jni/../../../build/include/QCAR/Vectors.h \
+ jni/../../../build/include/QCAR/Renderer.h \
+ jni/../../../build/include/QCAR/Matrices.h \
+ jni/../../../build/include/QCAR/State.h \
+ jni/../../../build/include/QCAR/Frame.h \
+ jni/../../../build/include/QCAR/VideoBackgroundConfig.h \
+ jni/../../../build/include/QCAR/Trackable.h \
+ jni/../../../build/include/QCAR/TrackableResult.h \
+ jni/../../../build/include/QCAR/Tool.h \
+ jni/../../../build/include/QCAR/Tracker.h \
+ jni/../../../build/include/QCAR/TrackerManager.h \
+ jni/../../../build/include/QCAR/ImageTracker.h \
+ jni/../../../build/include/QCAR/UpdateCallback.h \
+ jni/../../../build/include/QCAR/DataSet.h jni/SampleUtils.h \
+ jni/Texture.h jni/CubeShaders.h jni/Teapot.h jni/untitled.h jni/banana.h \
+ jni/A101.h jni/Banco.h jni/Aula.h jni/EdificioA.h jni/Biblioteca.h \
+ jni/Dispensadoras.h jni/DesechosEl.h jni/UVG.h
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/jni.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/assert.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2platform.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/KHR/khrplatform.h:
+
+C:/Users/Alex/Documents/Instaladores/Android/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/GLES2/gl2ext.h:
+
+jni/../../../build/include/QCAR/QCAR.h:
+
+jni/../../../build/include/QCAR/System.h:
+
+jni/../../../build/include/QCAR/CameraDevice.h:
+
+jni/../../../build/include/QCAR/NonCopyable.h:
+
+jni/../../../build/include/QCAR/VideoMode.h:
+
+jni/../../../build/include/QCAR/CameraCalibration.h:
+
+jni/../../../build/include/QCAR/Vectors.h:
+
+jni/../../../build/include/QCAR/Renderer.h:
+
+jni/../../../build/include/QCAR/Matrices.h:
+
+jni/../../../build/include/QCAR/State.h:
+
+jni/../../../build/include/QCAR/Frame.h:
+
+jni/../../../build/include/QCAR/VideoBackgroundConfig.h:
+
+jni/../../../build/include/QCAR/Trackable.h:
+
+jni/../../../build/include/QCAR/TrackableResult.h:
+
+jni/../../../build/include/QCAR/Tool.h:
+
+jni/../../../build/include/QCAR/Tracker.h:
+
+jni/../../../build/include/QCAR/TrackerManager.h:
+
+jni/../../../build/include/QCAR/ImageTracker.h:
+
+jni/../../../build/include/QCAR/UpdateCallback.h:
+
+jni/../../../build/include/QCAR/DataSet.h:
+
+jni/SampleUtils.h:
+
+jni/Texture.h:
+
+jni/CubeShaders.h:
+
+jni/Teapot.h:
+
+jni/untitled.h:
+
+jni/banana.h:
+
+jni/A101.h:
+
+jni/Banco.h:
+
+jni/Aula.h:
+
+jni/EdificioA.h:
+
+jni/Biblioteca.h:
+
+jni/Dispensadoras.h:
+
+jni/DesechosEl.h:
+
+jni/UVG.h:
---
+++ trunk/UVG-AR/src/com/qualcomm/QCARSamples/ImageTargets/GPSMap.java
@@ -0,0 +1,163 @@
+package com.qualcomm.QCARSamples.ImageTargets;
+
+
+
+import java.util.List;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+public class GPSMap extends MapActivity implements LocationListener, OnClickListener{
+	
+	private MapView mapView;
+	private Button mStartButton;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gps_screen);
+        
+        // Getting reference to MapView
+        mapView = (MapView) findViewById(R.id.map_view);
+        
+        // Setting Zoom Controls on MapView
+        mapView.setBuiltInZoomControls(true);
+        
+        
+        
+        // Getting LocationManager object from System Service LOCATION_SERVICE
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+        
+        // Getting the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
+        
+        // Getting Current Location
+        Location location = locationManager.getLastKnownLocation(provider);
+        
+        if(location!=null){
+        	onLocationChanged(location);
+        }
+        
+        locationManager.requestLocationUpdates(provider, 20000, 0, this);
+        
+        mStartButton = (Button) findViewById(R.id.button_realidadaumentada);
+        mStartButton.setOnClickListener(this);
+        
+    }
+	
+	private void startARActivity()
+    {
+        Intent i = new Intent(this, ImageTargets.class);
+        startActivity(i);
+    }
+
+
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+        case R.id.button_realidadaumentada:
+            startARActivity();
+            break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.gps_screen, menu);
+        return true;
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		TextView tvLocation = (TextView) findViewById(R.id.tv_location);
+		
+		// Getting latitude
+		double latitude = location.getLatitude();
+		
+		// Getting longitude
+		double longitude = location.getLongitude();
+		
+		// Setting latitude and longitude in the TextView tv_location
+		tvLocation.setText("Latitude:" +  latitude  + ", Longitude:"+ longitude );
+		
+		// Creating an instance of GeoPoint corresponding to latitude and longitude
+		GeoPoint point = new GeoPoint((int)(latitude * 1E6), (int)(longitude*1E6));
+		
+		// Getting MapController
+		MapController mapController = mapView.getController();
+		
+		// Locating the Geographical point in the Map
+		mapController.animateTo(point);
+		
+		// Applying a zoom
+		mapController.setZoom(15);
+		
+		// Redraw the map
+		mapView.invalidate();
+		
+		// Getting list of overlays available in the map
+		List<Overlay> mapOverlays = mapView.getOverlays();
+		
+		// Creating a drawable object to represent the image of mark in the map
+		Drawable drawable = this.getResources().getDrawable(R.drawable.cur_position);
+		
+		// Creating an instance of ItemizedOverlay to mark the current location in the map
+		CurrentLocationOverlay currentLocationOverlay = new CurrentLocationOverlay(drawable);
+		
+		// Creating an item to represent a mark in the overlay
+		OverlayItem currentLocation = new OverlayItem(point, "Current Location", "Latitude : " + latitude + ", Longitude:" + longitude);
+
+		// Adding the mark to the overlay
+		currentLocationOverlay.addOverlay(currentLocation);
+		
+		// Clear Existing overlays in the map
+		mapOverlays.clear();
+		
+		// Adding new overlay to map overlay
+		mapOverlays.add(currentLocationOverlay);		
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub		
+	}
+
+}
